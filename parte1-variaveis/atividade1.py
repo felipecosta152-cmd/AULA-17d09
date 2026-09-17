dcad6e24a33ef91329e7d8e531aff83c2e67a7bf
FelipeCo.2009 <felipe_costa152@estudante.sesisenai.org.br>
2026-09-17 08:59:49 -0300

    Adiciona o primeiro exercício de soma e interação com o usuário

---
+++ parte1-variaveis/atividade1.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ")
+print("Olá,", nome)
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+soma = numero1 + numero2
+print(f"A soma dos números é: {soma}")
+
+#input = texto
+#print = exibir na tela
+#float = número decimal
+# conta básica primeiro exercício
