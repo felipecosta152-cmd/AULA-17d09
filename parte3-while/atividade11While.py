819b868994a3056614f643beceafbb84bfa5a02e
FelipeCo.2009 <felipe_costa152@estudante.sesisenai.org.br>
2026-09-17 11:31:01 -0300

    Adiciona exercícios utilizando estruturas de repetição com while e for

---
+++ parte3-while/atividade11While.py
@@ -0,0 +1,11 @@
+numero = 1
+while numero <= 10:
+    print(numero)
+    numero = numero + 1
+#O while verifica se o número é menor ou igual a 10:
+#Aumentamos o número em: numero = numero + 1 
+
+# verifica se o numero é menor que 10,
+# se for ele entra na regra while 1+1 
+# quando for igual a 10 ele para por não ter <= 11 
+#então chega no limite e encerra. 
---
+++ parte3-while/atividadeextra.py
@@ -0,0 +1,7 @@
+for numero in range(1, 11):
+    print(numero)
+#range(1, 11) cria os números de 1 até 10.
+#Importante: o range não inclui o último número.
+#  Por isso usamos 11 para chegar até 10.
+
+
