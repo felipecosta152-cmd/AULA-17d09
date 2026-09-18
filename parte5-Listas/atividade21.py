a297ee40adeb4be9fa845200911ef4fa0a571fb8
Felipe.CO2009 <felipe_costa152@estudante.sesisenai.org.br>
2026-09-17 23:03:55 -0300

    Adiciona exercícios de listas para exibir elementos e calcular soma

---
+++ parte5-Listas/atividade21.py
@@ -0,0 +1,9 @@
+#LISTAS NAO
+#21. Crie uma lista com cinco números à sua escolha e exiba cada um deles.
+numeros = [10, 20, 30, 40, 50]
+for n in numeros: #o n é uma variavel que vai receber cada elemento da lista numeros.
+    print(n)
+    
+    # tipo ( n ) pega -> numeros 
+    # que é igual aos selecionados tlg...
+    
---
+++ parte5-Listas/atividade22.py
@@ -0,0 +1,13 @@
+#22. Usando a lista do exercício anterior, 
+# calcule e exiba a soma de todos os itens.
+
+numeros = [10, 20, 30, 40, 50]  # for repetira 5 vezes 
+
+soma = 0 #armazena
+
+for n in numeros:
+    soma = soma + n   # n = variaveis em numero
+
+print("Soma total:", soma)          #:)
+
+#codigo nao ta funcionando não sei oque deu 
